8112f5ec56926cae3a0b16f34a9edfec8662167d
pyPlacca <dhegreatprosperity10@gmail.com>
2020-04-24 14:15:34 +0000

    refactored code

--- player.js
+++ player.js
@@ -9,54 +9,13 @@ const [prog, video, btn, cur, dur, marker] = [
 	".prog .marker",
 ].map((arg) => document.querySelector(arg));
 
-function updateProg(e) {
-	prog.max = e.target.duration;
-	dur.innerText = formatTime(e.target.duration);
-	// update title
-	document.querySelector(".title").innerText = 
-		e.target.currentSrc
-			.split("/")
-			.pop();
-}
-
-function updatePlaybackProgress(e) {
-	prog.value = e.target.currentTime;
-	cur.innerText = formatTime(e.target.currentTime);
-}
-
-function getTime(e) {
-	let percent = (e.offsetX * 100) / prog.clientWidth;
-	return (percent / 100) * video.duration;
-}
-
+// jump playback forwards or backwards
 function adjustPlayback(e) {
-	// jump playback forwards or backwards
 	video.currentTime = getTime(e);
 }
 
-function showMarker() {
-	const mark_time = marker.firstElementChild;
-	mark_time.style.left = -mark_time.clientWidth + "px";
-	marker.style.transform = "scale(1)";
-}
-
-function moveMarker(e) {
-	// show playback time at marker position
-	marker.firstElementChild.innerText = formatTime(getTime(e));
-	// re-position playback marker
-	marker.style.left =
-		e.offsetX + 
-		e.target.offsetLeft - 
-		marker.clientWidth / 2 + 
-		"px";
-}
-
-function hideMarker() {
-	marker.style.transform = "scale(0)";
-}
-
+// convert seconds to HH:MM:ss time format
 function formatTime(secs) {
-	// convert seconds to HH:MM:ss time format
 	return new Date(secs * 1000)
 		.toISOString()
 		.substr(11, 8);
@@ -65,20 +24,55 @@ function formatTime(secs) {
 	// .join(":")
 }
 
-function togglePlay(e) {
-	// switch between pause and play
+function getTime(e) {
+	let percent = (e.offsetX * 100) / prog.clientWidth;
+	return (percent / 100) * video.duration;
+}
+
+function hideMarker() {
+	marker.style.transform = "scale(0)";
+}
+
+// follow cursor and re-position playback marker
+function moveMarker(e) {
+	// show playback time at current mark
+	marker.firstElementChild.innerText = formatTime(getTime(e));
+	marker.style.left = e.offsetX + e.target.offsetLeft - marker.clientWidth / 2 + "px";
+}
+
+function showMarker() {
+	marker.style.transform = "scale(1)";
+}
+
+// toggles play/pause icon
+function toggleBtn(e) {
+	e.target.previousElementSibling.id = "play";
+}
+
+// switch between pause and play states
+function togglePlay() {
 	if (video.paused) {
 		video.play();
 		btn.id = "pause";
+		return
 	} else {
 		video.pause();
 	}
 }
 
-function toggleBtn(e) {
-	// toggles play/pause icon
-	e.target.previousElementSibling.id = "play";
+function updatePlaybackProgress(e) {
+	prog.value = e.target.currentTime;
+	cur.innerText = formatTime(e.target.currentTime);
 }
+
+function updateProg(e) {
+	prog.max = e.target.duration;
+	dur.innerText = formatTime(e.target.duration);
+	// update title
+	document.querySelector(".title").innerText = 
+		e.target.currentSrc.split("/").pop();
+}
+
 /*
 // Drag and Drop Support
 'drag dragstart dragend dragover dragenter dragleave drop'
@@ -109,7 +103,7 @@ cur.innerText = formatTime(0);
 marker.style.width = marker.style.height = prog.clientHeight + "px";
 marker.style.top = prog.offsetTop + "px";
 // play/pause key binding
-document.onkeypress = (key) => {
+document.onkeypress = key => {
 	key.code === "Space" ? togglePlay() : null
 }
 
